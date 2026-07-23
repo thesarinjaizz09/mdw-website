d5c65af01f019f65c700068c6a00f24484c18157
thesarinjaizz09 <jazzsarin28@gmail.com>
2026-07-23 16:04:28 +0530

    feast: prescription user specific upload

---
+++ app/api/upload/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { backendFetch } from "@/lib/server/backend";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const response = await backendFetch(
+    `/prescription`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }
+  );
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    return NextResponse.json(
+      { message: data.message },
+      { status: response.status }
+    );
+  }
+
+
+  return NextResponse.json({
+    success: true
+  });
+}
